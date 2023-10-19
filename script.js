708d87ea719717f61c6d0a8a2b4980349328054d
João Giovani <giovannirs957@gmail.com>
2023-10-18 21:11:14 -0300

    function generateCircle created

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const scissors = document.querySelector(".father-circles-sci");
 const rock = document.querySelector(".father-circles-rock");
 const triangle = document.querySelector(".triangle");
 const picked = document.querySelectorAll(".picked");
+const boxShadow = document.querySelector(".box-shadow");
+const options = document.querySelector(".options");
 
 const circles = document.querySelectorAll(".all");
 
@@ -15,8 +17,16 @@ function anime() {
     picked.forEach((i) => {
       setTimeout(function () {
         i.classList.toggle("ocult");
-      }, 1000);
+      }, 300);
     });
+    if (this.classList.contains("animaPaper")) {
+      boxShadow.style.opacity = 1;
+      boxShadow.style.zIndex = 1;
+    } else {
+      boxShadow.style.opacity = 0;
+    }
+    generateCircle();
+
   }
 
   if (this === scissors) {
@@ -27,8 +37,16 @@ function anime() {
     picked.forEach((i) => {
       setTimeout(function () {
         i.classList.toggle("ocult");
-      }, 1000);
+      }, 300);
     });
+    if (this.classList.contains("animaScissors")) {
+      boxShadow.style.opacity = 1;
+      boxShadow.style.zIndex = 1;
+    } else {
+      boxShadow.style.opacity = 0;
+    }
+    generateCircle();
+
   }
   if (this === rock) {
     this.classList.toggle("animaRock");
@@ -38,11 +56,41 @@ function anime() {
     picked.forEach((i) => {
       setTimeout(function () {
         i.classList.toggle("ocult");
-      }, 2000);
+      }, 300);
     });
+    if (this.classList.contains("animaRock")) {
+      boxShadow.style.opacity = 1;
+      boxShadow.style.zIndex = 1;
+    } else {
+      boxShadow.style.opacity = 0;
+    }
+    generateCircle();
   }
 }
 
 circles.forEach((i) => {
   i.addEventListener("click", anime);
 });
+
+function generateCircle() {
+  let genNumber = Math.floor(Math.random() * 3) + 1;
+  if (genNumber === 1) {
+    const clonePaper = paper.cloneNode(true);
+    clonePaper.id = "clonePaper";
+    options.appendChild(clonePaper);
+    clonePaper.classList.remove("ocult");
+  }
+  if (genNumber === 2) {
+    const cloneScissor = scissors.cloneNode(true);
+    cloneScissor.id = "cloneScissor";
+    options.appendChild(cloneScissor);
+    cloneScissor.classList.remove("ocult");
+  }
+  if (genNumber === 3) {
+    const cloneRock = rock.cloneNode(true);
+    cloneRock.id = "cloneRock";
+    options.appendChild(cloneRock);
+    cloneRock.classList.remove("ocult");
+  }
+  
+}
